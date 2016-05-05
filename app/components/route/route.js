8f7d8f91dd0c43e6814eecf4a100bb22c4c6ac5e
Tomas Caufriez Sanchez <tocausan@gmail.com>
2016-05-05 11:34:23 +0800

    translations and views finalization

--- app/components/route/route.js
+++ app/components/route/route.js
@@ -4,11 +4,11 @@ angular.module('myApp.route', ['ngRoute'])
 
     .config(['$routeProvider', function($routeProvider) {
         $routeProvider.when('/view1', {
-            templateUrl: 'partials/view1.html',
+            templateUrl: 'components/partials/view1.html',
             controller: 'view1Ctrl'
         });
         $routeProvider.when('/view2', {
-            templateUrl: 'partials/view2.html',
+            templateUrl: 'components/partials/view2.html',
             controller: 'view2Ctrl'
         });
     }])
--- app/components/translation/translation-fr-fr.js
+++ app/components/translation/translation-fr-fr.js
@@ -12,7 +12,7 @@ angular.module('myApp.translation-fr-fr', [])
             Intro_part_1:   'Bienvenue sur Multinlingual Angular App.',
             Intro_part_2:   'Cette application est destinée à offrir un service international.',
 
-            About_part_1:   'Cette application est basée sur une architecture AngularJS utilisant "angular-route" pour le routing et "angular-translate" (Pascal Precht) pour la traduction. ' +
+            About_part_1:   'Cette application est basée sur l\'architecture d\'"angular-seed" et utilisant "angular-route" pour le routing et "angular-translate" (Pascal Precht) pour la traduction. ' +
             'Ces noms font références aux paquets "Bower".',
             About_part_2:   '"Foundation" est aussi utilisé afin de faciliter la rapide mise en place d\'une interface utilisateur agréable.',
         });
